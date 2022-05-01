a010b0a8bb9926db108c7f62ab43001b319faa48
saiffardin <saiffardin@gmail.com>
2022-05-01 21:12:35 +0600

    will use cards to to show 10 items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import {useEffect, useState} from 'react';
+// import {Card, Col, Row} from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -8,6 +11,7 @@ function App() {
 
     const [data, setData] = useState([]);
     let url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_API}`;
+    let posterUrl = `https://image.tmdb.org/t/p/w500`;
 
     useEffect(() => {
         fetch(url)
@@ -27,14 +31,36 @@ function App() {
             <h1>Kona SL</h1>
 
             <h3>Length:{data?.length}</h3>
-            {
+
+            {/* <Row xs={1} md={2} lg={5} className="g-4">
+                {Array.from({length: 10}).map((_, idx) => (
+                    <Col>
+                        <Card style={{width: '15rem'}}>
+
+                            <Card.Img variant="top" src="https://image.tmdb.org/t/p/w500/zmXaK4ajfTUOeb8K13uQ4fNfE8L.jpg" className='posterImg' />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+
+                                <Card.Text>
+
+                                    <Card.Subtitle className='my-2'>Release Date: <span className=' fw-light' >cxcx</span></Card.Subtitle>
+                                    <Card.Subtitle className='my-2'>Genre:</Card.Subtitle>
+                                    <Card.Subtitle className='my-2'>Ratings:</Card.Subtitle>
+
+                                </Card.Text>
 
 
-            }
-
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row> */}
 
         </div>
     );
 }
+
+
+
 
 export default App;
---
+++ src/Components/ShowTenMovies/ShowTenMovies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowTenMovies = () => {
+    return (
+        <div>
+            <h1>ShowTenMovies</h1>
+        </div>
+    );
+};
+
+export default ShowTenMovies;
