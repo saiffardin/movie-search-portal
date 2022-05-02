8288506b4166e379efac91a31060f98890b0f3c4
saiffardin <saiffardin@gmail.com>
2022-05-02 22:04:38 +0600

    search basic ui developed

--- src/Components/ShowAllMovies/ShowAllMovies.js
+++ src/Components/ShowAllMovies/ShowAllMovies.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import React, {useEffect, useState} from 'react';
-import {Col, Row} from 'react-bootstrap';
+import {Col, Dropdown, Row} from 'react-bootstrap';
 import ReactPaginate from 'react-paginate';
 import SingleMovie from '../SingleMovie/SingleMovie';
 
@@ -40,6 +40,25 @@ const ShowAllMovies = (props) => {
             {/* Section Title */}
             <h1 className={`text-center ${divBg !== 'top-rated' && 'text-white'}`}>All {sectionTitle}</h1>
 
+            <div className='d-flex justify-content-center'>
+                <Dropdown className='m-2'>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Filter By
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item>Movie Name</Dropdown.Item>
+                        <Dropdown.Item>Release Date</Dropdown.Item>
+                        <Dropdown.Item>Genre</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <input
+                    className="search m-2"
+                    placeholder="Search..."
+                />
+            </div>
+
             {/* Cards - Movies */}
             <Row lg={5} >
                 {movies.map((movie, index) => (
@@ -50,7 +69,7 @@ const ShowAllMovies = (props) => {
                             genres={genres}
                             showDetails={true}
                         />
-                       
+
                     </Col >
                 ))}
             </Row >
