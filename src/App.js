fabe262b92269417ccb5d8f627523c993a4d7029
saiffardin <saiffardin@gmail.com>
2022-05-02 13:37:20 +0600

    react router initial setup done

--- src/App.js
+++ src/App.js
@@ -1,114 +1,38 @@
 /* eslint-disable no-unused-vars */
-
 /* eslint-disable react-hooks/exhaustive-deps */
-import {useEffect, useState} from 'react';
+import {
+    BrowserRouter as Router, Route, Switch
+} from "react-router-dom";
 import './App.css';
-import ShowAllMovies from './Components/ShowAllMovies/ShowAllMovies';
-// import ShowTenMovies from './Components/ShowTenMovies/ShowTenMovies';
 
 function App() {
-    /** data.results er 'keys' - 15
-     * ['id', 'video', 'vote_average', 
-     * 'overview', 'release_date', 'title', 
-     * 'adult', 'backdrop_path', 'vote_count', '
-     * genre_ids', 'poster_path', 'original_language', 
-     * 'original_title', 'popularity', 'media_type']
-     */
 
-    const apiKey = process.env.REACT_APP_API;
-
-    const [genres, setGenres] = useState([]);
-    const [trendingMovies, setTrendingMovies] = useState([]);
-    const [topRatedMovies, setTopRatedMovies] = useState([]);
-    const [upcomingMovies, setUpcomingMovies] = useState([]);
-
-    const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
-    const trendingMoviesUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`;
-    const topRatedMoviesUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
-    const upcomingMoviesUrl = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`;
-
-
-
-    useEffect(() => {
-
-        fetch(genreUrl)
-            .then((data) => data.json())
-            .then((res) => setGenres(res.genres))
-
-        fetch(trendingMoviesUrl)
-            .then((data) => data.json())
-            .then((res) => setTrendingMovies(res.results.slice(0, 10)))
-
-        fetch(topRatedMoviesUrl)
-            .then((data) => data.json())
-            .then((res) => setTopRatedMovies(res.results.slice(0, 10)))
-
-        fetch(upcomingMoviesUrl)
-            .then((data) => data.json())
-            .then((res) => setUpcomingMovies(res.results.slice(0, 10)))
-    }, [])
 
 
     // console.log(genres);
 
     return (
-        <div>
-            <h1>App </h1>
+        <Router>
+            <div>
 
-            {/* Trending Movies */}
-            {/* <ShowTenMovies
-                genres={genres}
-                divBg="trending"
-                sectionTitle="Trending Movies"
-                movies={trendingMovies}
-            /> */}
+                {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route exact path="/">
+                        <h2>Home</h2>
+                    </Route>
+                    <Route path="/about">
+                        <h2>About</h2>
+                    </Route>
+                    <Route path="/users">
+                        <h2>Users</h2>
+                    </Route>
 
-            {/* Top Rated Movies */}
-            {/* <ShowTenMovies
-                genres={genres}
-                divBg="top-rated"
-                sectionTitle="Top Rated Movies"
-                movies={topRatedMovies}
-            /> */}
-
-            {/* Upcoming Movies */}
-            {/* <ShowTenMovies
-                genres={genres}
-                divBg="upcoming"
-                sectionTitle="Upcoming Movies"
-                movies={upcomingMovies}
-            /> */}
-
-            {/* ------------------------------------------------- */}
-
-            {/* show all - trending */}
-            {/* <ShowAllMovies
-                genres={genres}
-                divBg="trending"
-                sectionTitle="Trending Movies"
-                url={trendingMoviesUrl}
-            /> */}
-
-            {/* show all - top-rated */}
-            {/* <ShowAllMovies
-                genres={genres}
-                divBg="top-rated"
-                sectionTitle="Top Rated Movies"
-                url={topRatedMoviesUrl}
-            /> */}
-
-            {/* show all - upcoming */}
-            {/* <ShowAllMovies
-                genres={genres}
-                divBg="upcoming"
-                sectionTitle="Upcoming Movies"
-                url={upcomingMoviesUrl}
-            /> */}
-
-        </div>
+                </Switch>
+            </div>
+        </Router>
     );
 }
-
 
 
 
