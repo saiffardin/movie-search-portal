85995daedf9a65acc218e8481f3c563732247b38
saiffardin <saiffardin@gmail.com>
2022-05-02 17:48:58 +0600

    comments added

--- src/utility/genre.js
+++ src/utility/genre.js
@@ -3,3 +3,28 @@ export const getGenreNameById = (arr, id) => {
     // console.log(found?.name);
     return found?.name;
 }
+
+/* ja ja baad dibo from showing movie details
+    {
+        "adult": false,
+        "backdrop_path": "/9SISFlf6SEiIb7CMk8h0Gu3NYow.jpg",
+        "id": 745881,
+        "media_type": "movie"
+        "video": false,
+    }
+*/
+
+
+/*
+In movie detail page you have to show 
+    movie poster, 
+    title, 
+    genres, 
+    overview, 
+    production company name, 
+    release date, 
+    cast crew with 
+        profile image 
+        and name, 
+    video trailer.
+*/
