a8d175596fe843127f7c241889e94664402c178e
saiffardin <saiffardin@gmail.com>
2022-05-05 04:59:51 +0600

    index.css deleted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
