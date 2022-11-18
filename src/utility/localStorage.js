95d8c8a32dd7ba8439891f4d506ec04f25836e40
Saif Chowdhury <saiffardin@gmail.com>
2022-11-18 21:37:44 +0600

    localStorage.js - refactored

--- src/utility/localStorage.js
+++ src/utility/localStorage.js
@@ -1,27 +1,12 @@
 export const addItemToLocalStorage = (item) => {
     let wishlistArr = getItemFromLocalStorage();
+    wishlistArr = [...wishlistArr, item];
 
-    if (wishlistArr) {
-        console.log('not NULL - curr wishlistArr:', wishlistArr);
-        console.log('-------');
-
-        wishlistArr = [...wishlistArr, item];
-        console.log('%cNew - added wishlistArr:', 'color:yellow', wishlistArr);
-        localStorage.setItem('wishlist', JSON.stringify(wishlistArr));
-    }
-
-    // when local storage is empty
-    else {
-        console.log('%cNULL - wishlistArr:', 'color:red', wishlistArr);
-        localStorage.setItem('wishlist', JSON.stringify([item]));
-    }
+    localStorage.setItem('wishlist', JSON.stringify(wishlistArr));
 }
 
-
 export const getItemFromLocalStorage = () => {
-
     let wishlistArr = JSON.parse(localStorage.getItem('wishlist'));
-
     return wishlistArr || [];
 }
 
@@ -29,16 +14,13 @@ export const isItemAlreadyInLocalStorage = (newItem) => {
     const itemsInWishlist = getItemFromLocalStorage();
 
     const resultArr = itemsInWishlist.filter((obj => obj.id === newItem.id));
-
     return resultArr.length === 0 ? false : true;
 }
 
 export const removeItemFromLocalStorage = (removeItem) => {
-
     const itemsInWishlist = getItemFromLocalStorage();
 
     const updatedWishlist = itemsInWishlist.filter((obj => obj.id !== removeItem.id));
+
     localStorage.setItem('wishlist', JSON.stringify(updatedWishlist));
-
-
 }
