b0c4e9a4cc2f7e10c1641444949faad80940a229
Saif Chowdhury <saiffardin@gmail.com>
2025-05-06 03:50:56 +0600

    delete legacy files

--- src/utility/apiUrls.js
+++
@@ -1,9 +0,0 @@
-const apiKey = process.env.REACT_APP_API;
-
-export const getGenresUrl = () => `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
-
-export const getMoviesByGenreUrl = (genreId) => `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`;
-
-export const getMovieDetailsUrl = (movieId) => `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
-
-export const getMoviePosterUrl = (path) => `https://image.tmdb.org/t/p/w500${path}`
